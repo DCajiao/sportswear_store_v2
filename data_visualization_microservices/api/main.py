9879c9c7df99da7285c523e7313c6ec53bbae4b2
DCajiao <dcajiaolazt@gmail.com>
2024-05-14 17:03:10 -0500

    KEEPALIVE function added to the API and folder /Notebooks added

--- data_visualization_microservices/api/main.py
+++ data_visualization_microservices/api/main.py
@@ -3,3 +3,19 @@
 # 1. actualizar las gráficas en tiempo real.
 # 2. cargarlas en la página web.
 # 3. Retornarlas a POSTMAN.
+
+from flask import Flask, jsonify, send_file
+
+#----------------------------------------------------------------
+app = Flask(__name__)
+#----------------------------------------------------------------
+
+@app.route('/KEEPALIVE')
+def mainpage():
+    msg= "<center><h1>If you can see this message, it means that the API is still alive.</h1></center>"
+    return msg
+
+# ----------------------------------------------------------------
+
+if __name__ == '__main__': 
+    app.run(debug=True, host='0.0.0.0')
