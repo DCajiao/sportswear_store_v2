fc8a69458ba66c02cb860c3e823f9091c7163e46
natam226 <natalia.moreno_mon@uao.edu.co>
2024-05-26 09:48:42 -0500

    Elimination of an exception

--- backend/src/main/java/com/apiweb/backend/Exception/TallaNoEncontradaException.java
+++
@@ -1,7 +0,0 @@
-package com.apiweb.backend.Exception;
-
-public class TallaNoEncontradaException extends RuntimeException{
-    public TallaNoEncontradaException(String mensaje){
-        super(mensaje);
-    }
-}
